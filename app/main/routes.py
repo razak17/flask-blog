ac4aa30168d6e6a150aa94edbbea3385b9a04b53
Abdul Razak <moabdulrazak17@gmail.com>
2020-03-08 11:21:27 +0000

    pagination

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, flash, redirect, url_for, cur
 from app.main.forms import UpdateAccountForm, PostForm
 from flask_login import login_required, current_user
 from app.models import User, Post
+from config import Config
 from app.main import bp
 from PIL import Image
 from app import db
@@ -15,6 +16,8 @@ import os
 @login_required
 def index():
     posts = Post.query.all()
+    page =request.args.get("page", 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page, current_app.config['POSTS_PER_PAGE'])
     return render_template("main/index.html", posts=posts)
 
 
@@ -106,3 +109,13 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('main.index'))
+
+
+@bp.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page, current_app.config['POSTS_PER_PAGE'])
+    return render_template('main/user_posts.html', posts=posts, user=user)
